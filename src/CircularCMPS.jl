689702a2eba5d4a172ff1a54048bdd7956bdbb6c
Wei Tang <wei.tang.nju@gmail.com>
2025-02-20 18:11:21 +0100

    minor

--- src/CircularCMPS.jl
+++ src/CircularCMPS.jl
@@ -80,11 +80,11 @@ include("operators.jl");
 include("ground_state.jl")
 include("excited_state.jl");
 include("continuous_bosons/multi_boson_cmps.jl");
+include("continuous_bosons/multi_boson_cmps_P.jl");
 include("continuous_bosons/multi_boson_cmps_MDMinv.jl");
 include("continuous_bosons/multi_boson_cmps_tnp.jl");
 #include("continuous_bosons/multi_boson_cmps_MCMinv.jl");
 include("continuous_bosons/hamiltonian_zoo.jl")
-include("continuous_bosons/multi_boson_cmps_P.jl");
 include("transfer_matrix.jl");
 include("optim_alg.jl");
 
