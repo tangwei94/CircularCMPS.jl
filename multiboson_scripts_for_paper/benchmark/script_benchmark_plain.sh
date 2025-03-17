4c0555c7d9cce5f434867abaf29e76a95b5aa776
Wei Tang <wei.tang.nju@gmail.com>
2025-03-17 11:41:14 +0100

    remove useless file

--- multiboson_scripts_for_paper/benchmark/script_benchmark_plain.sh
+++
@@ -1,7 +0,0 @@
-for chi in 4 6 8 ; do
-    for ix in {1..20} ; do  
-        echo $ix ; 
-        julia --project=. multiboson_scripts_for_paper/benchmark/benchmark_plain.jl $chi $ix ; 
-    done
-done
-
