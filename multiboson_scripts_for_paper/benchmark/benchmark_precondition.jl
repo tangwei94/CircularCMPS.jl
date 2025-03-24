67a561de0ce26280cb436eadb7fd183400ed56c3
Wei Tang <tangwei@smail.nju.edu.cn>
2025-03-24 15:08:57 +0100

    benchmark_precondition.jl

--- multiboson_scripts_for_paper/benchmark/benchmark_precondition.jl
+++ multiboson_scripts_for_paper/benchmark/benchmark_precondition.jl
@@ -17,23 +17,23 @@ c12 = 0.5
 
 Hm = MultiBosonLiebLiniger([c1 c12; c12 c2], [μ1, μ2], Inf) 
 
-function optimization_plain(χ::Integer)
+function optimization_precondition(χ::Integer)
     ys, gs = Float64[], Float64[]
     myfinalize!(x, f, g, numiter) = begin
         push!(ys, f)
         push!(gs, norm(g))
         return x, f, g, numiter
     end
-    ψ0 = MultiBosonCMPSData_diag(rand, χ, 2);
-    res = ground_state(Hm, ψ0; gradtol=1e-6, maxiter=1000000, do_preconditioning=false, _finalize! =myfinalize!);
+    ψ0 = MultiBosonCMPSData_MDMinv(rand, χ, 2);
+    res_opt = ground_state(Hm, ψ0; gradtol=1e-6, maxiter=100000, preconditioner_type=1, _finalize! =myfinalize!);
     return ys, gs
 end
 
 χ = parse(Int, ARGS[1])
 ix = parse(Int, ARGS[2])
-res = optimization_plain(χ);
-#@save "multiboson_scripts_for_paper/benchmark/data/benchmark_plain_opt_$(χ)_$(ix).jld2" res
-@save "benchmark_plain_opt_$(χ)_$(ix).jld2" res
+res = optimization_precondition(χ);
+#@save "multiboson_scripts_for_paper/benchmark/data/benchmark_precondition_opt_$(χ)_$(ix).jld2" res
+@save "benchmark_precondition_opt_$(χ)_$(ix).jld2" res
 
 
 
