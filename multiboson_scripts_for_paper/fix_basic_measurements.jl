8ef0bc4a3af132381ba4f64eb88f9b63667875b1
Wei Tang <tangwei@smail.nju.edu.cn>
2025-09-18 13:09:38 +0200

    fix basic measurements

--- multiboson_scripts_for_paper/fix_basic_measurements.jl
+++ multiboson_scripts_for_paper/fix_basic_measurements.jl
@@ -17,25 +17,24 @@ println("Directories in $root_folder:")
 if isdir(root_folder)
     directories = filter(isdir, readdir(root_folder, join=true))
     for (i, dir) in enumerate(directories)
-        dir_name = basename(dir)
-        println("$i. $dir_name")
+        folder_name = basename(dir)
+        println("fixing basic measurements for $folder_name")
+        
+        open(joinpath(root_folder, folder_name, "basic_measurements.txt"), "w") do f
+            println(f, "chi, energy, gnorm, n1, n2, num_iter")
+            for χ in [4, 8, 16, 32]
+                required_file = joinpath(root_folder, folder_name, "results_chi$(χ).jld2")
+                if isfile(required_file)
+                    @load required_file res
+                    n1 = particle_density(res[1], 1)
+                    n2 = particle_density(res[1], 2)
+                    num_iter = size(res[5])[1]
+                    msg = "$χ, $(res[2]), $(norm(res[3])), $n1, $n2, $num_iter"
+                    println(f, msg)
+                    println(msg)
+                end
+            end
+        end
     end
     println("Total directories found: $(length(directories))")
-else
-    println("Root folder '$root_folder' does not exist!")
 end
-
-#Hm = MultiBosonLiebLiniger([c1 c12; c12 c2], [μ1, μ2], Inf);
-#
-## some basic measurements 
-#open(joinpath(root_folder, folder_name, "basic_measurements.txt"), "w") do f
-#    println(f, "chi, energy, gnorm, n1, n2, num_iter")
-#    for (res, χ) in zip([res1, res2, res3, res4], [4, 8, 16, 32])
-#        n1 = particle_density(res[1], 1)
-#        n2 = particle_density(res[1], 2)
-#        num_iter = size(res[5])[1]
-#        msg = "$χ, $(res[2]), $(norm(res[3])), $n1, $n2, $num_iter"
-#        println(f, msg)
-#        println(msg)
-#    end
-#end
