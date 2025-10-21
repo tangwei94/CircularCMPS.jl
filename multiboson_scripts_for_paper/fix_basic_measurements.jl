15f1aadfef794a7a9560cdc98f3c23786d4484d6
Wei Tang <tangwei@smail.nju.edu.cn>
2025-10-22 01:22:58 +0200

    update

--- multiboson_scripts_for_paper/fix_basic_measurements.jl
+++ multiboson_scripts_for_paper/fix_basic_measurements.jl
@@ -18,20 +18,22 @@ if isdir(root_folder)
     directories = filter(isdir, readdir(root_folder, join=true))
     for (i, dir) in enumerate(directories)
         folder_name = basename(dir)
-        println("fixing basic measurements for $folder_name")
+        if occursin("mu1.0_", folder_name)
+            println("fixing basic measurements for $folder_name")
         
-        open(joinpath(root_folder, folder_name, "basic_measurements.txt"), "w") do f
-            println(f, "chi, energy, gnorm, n1, n2, num_iter")
-            for χ in [4, 8, 16, 32]
-                required_file = joinpath(root_folder, folder_name, "results_chi$(χ).jld2")
-                if isfile(required_file)
-                    @load required_file res
-                    n1 = particle_density(res[1], 1)
-                    n2 = particle_density(res[1], 2)
-                    num_iter = size(res[5])[1]
-                    msg = "$χ, $(res[2]), $(norm(res[3])), $n1, $n2, $num_iter"
-                    println(f, msg)
-                    println(msg)
+            open(joinpath(root_folder, folder_name, "basic_measurements.txt"), "w") do f
+                println(f, "chi, energy, gnorm, n1, n2, num_iter")
+                for χ in [4, 8, 16, 32]
+                    required_file = joinpath(root_folder, folder_name, "results_chi$(χ).jld2")
+                    if isfile(required_file)
+                        @load required_file res
+                        n1 = particle_density(res[1], 1)
+                        n2 = particle_density(res[1], 2)
+                        num_iter = size(res[5])[1]
+                        msg = "$χ, $(res[2]), $(norm(res[3])), $n1, $n2, $num_iter"
+                        println(f, msg)
+                        println(msg)
+                    end
                 end
             end
         end
