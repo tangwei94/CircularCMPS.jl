3c24a5fdc6286d240e84841c9bbac3b049d49632
Wei Tang <tangwei@smail.nju.edu.cn>
2025-10-23 19:39:46 +0200

    minor

--- multiboson_scripts_for_paper/two_component_lieb_liniger.jl
+++ multiboson_scripts_for_paper/two_component_lieb_liniger.jl
@@ -12,7 +12,7 @@ using CircularCMPS
 
 # parameters for the model, only consider equal mass for now
 c = 10.0
-#μ, c12 = 0.0, -7.0
+μ, c12 = 0.0, -2.0
 μ = parse(Float64, ARGS[1])
 c12 = parse(Float64, ARGS[2])
 
@@ -47,14 +47,14 @@ res_d3 = ground_state(Hm, ϕ3; gradtol=1e-12, maxiter=1000, do_preconditioning=f
 res3 = ground_state(Hm, ψ3; gradtol=1e-6, maxiter=5000, preconditioner_type=3);
 @save joinpath(root_folder, folder_name, "results_chi16.jld2") res=res3
 
-ψ4 = left_canonical(expand(res3[1], res2[1]; perturb=0.025));
+ψ4 = left_canonical(expand(res3[1], res2[1]; perturb=1e-4));
 ϕ4 = MultiBosonCMPSData_diag(ψ4)
 res_d4 = ground_state(Hm, ϕ4; gradtol=1e-12, maxiter=1000, do_preconditioning=false);
 ψ4 = left_canonical(MultiBosonCMPSData_MDMinv(res_d4[1]))
 res4 = ground_state(Hm, ψ4; gradtol=1e-6, maxiter=5000, preconditioner_type=3);
 @save joinpath(root_folder, folder_name, "results_chi24.jld2") res=res4
 
-ψ5 = left_canonical(expand(res4[1], res2[1]; perturb=0.025));
+ψ5 = left_canonical(expand(res4[1], res2[1]; perturb=1e-4));
 ϕ5 = MultiBosonCMPSData_diag(ψ5)
 res_d5 = ground_state(Hm, ϕ5; gradtol=1e-12, maxiter=1000, do_preconditioning=false);
 ψ5 = left_canonical(MultiBosonCMPSData_MDMinv(res_d5[1]))
