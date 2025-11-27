9dde918a5ae7b25b4ae22fa65464a47bca895201
Wei Tang <wei.tang.nju@gmail.com>
2025-11-27 23:19:12 +0100

    perturbing

--- multiboson_scripts_for_paper/two_component_lieb_liniger_perturbing.jl
+++ multiboson_scripts_for_paper/two_component_lieb_liniger_perturbing.jl
@@ -18,7 +18,7 @@ c = 1.0
 μ = parse(Float64, ARGS[1])
 c12 = parse(Float64, ARGS[2])
 
-δμ = 0.01
+δμ = 0.001
 
 c1, c2 = c, c
 μ1, μ2 = μ, μ 
@@ -29,13 +29,13 @@ root_folder = "data_two_component_lieb_liniger"
 folder_name = "results_c$(c)_mu$(μ)_coupling$(c12)"
 mkpath(root_folder)
 
-label_meaning = Dict('N' => -2.0, 'n' => -1.5, 'M' => -1.0, 'm' => -0.5, '0' => 0.0, 'p' => 0.5, 'P' => 1.0, 'q' => 1.5, 'Q' => 2.0)
+label_meaning = Dict('N' => -4.0, 'n' => -3.0, 'M' => -2.0, 'm' => -1.0, '0' => 0.0, 'p' => 1.0, 'P' => 2.0, 'q' => 3.0, 'Q' => 4.0)
 
 # before running the job, delete all the directories in root_folder except folder_name
 # otherwise, different jobs might conflict with eachother when copying data to the management node
 # TODO. read the datafile first, and then delete everything
 labels = ["NN", "nn", "MM", "mm", "pp", "PP", "qq", "QQ", "mp", "MP", "nq", "NQ"]
-files_to_keep = ["perturbing_$(perturbing_label)_1e-2_results_c$(c)_mu$(μ)_coupling$(c12)" for perturbing_label in labels]
+files_to_keep = ["perturbing_$(perturbing_label)_1e-3_results_c$(c)_mu$(μ)_coupling$(c12)" for perturbing_label in labels]
 @info "Cleaning up directories in $root_folder..."
 mkpath("tmp_init")
 # copy jld2 files in folder_name to tmp_init 
@@ -63,7 +63,7 @@ for perturbing_label in labels
     μ2_perturbing = μ2 + s2 * δμ
     Hm = MultiBosonLiebLiniger([c1 c12; c12 c2], [μ1_perturbing, μ2_perturbing], Inf);
     
-    folder_name_perturbing = "perturbing_$(perturbing_label)_1e-2_results_c$(c)_mu$(μ)_coupling$(c12)"
+    folder_name_perturbing = "perturbing_$(perturbing_label)_1e-3_results_c$(c)_mu$(μ)_coupling$(c12)"
     mkpath(joinpath(root_folder, folder_name_perturbing))
     open(joinpath(root_folder, folder_name_perturbing, "basic_measurements.txt"), "w") do f
         println(f, "chi, energy, gnorm, n1, n2, num_iter")
@@ -72,7 +72,7 @@ for perturbing_label in labels
     for (χ, file) in zip([4, 8, 16, 32], ["results_chi4.jld2", "results_chi8.jld2", "results_chi16.jld2", "results_chi32.jld2"])
         @load joinpath("tmp_init", file) res
         ψ1 = deepcopy(res[1])
-        ψ1 = left_canonical(CircularCMPS.perturb(ψ1; perturb=1e-4))
+        #ψ1 = left_canonical(CircularCMPS.perturb(ψ1; perturb=1e-6))
         res1 = ground_state(Hm, ψ1; gradtol=1e-6, maxiter=10000, preconditioner_type=3);
         @save joinpath(root_folder, folder_name_perturbing, file) res=res1
 
